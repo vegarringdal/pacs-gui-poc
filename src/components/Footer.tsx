4c3ff88f7d772cfe904d07e40495d54654626c1a
Vegar Ringdal <vegar.ringdal@gmail.com>
2022-10-09 22:30:17 +0200

    chore: make header and footer use all space

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function Footer() {
   return (
-    <div className="flex flex-col bg-gray-900">
+    <div className="flex flex-col bg-gray-900 w-full h-full overflow-hidden">
       <div className="m-auto text-2xl text-gray-200 font-semibold p-2"> PTOC - Project Tag Operation Control</div>
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ export function Header() {
   document.title = `${title}`;
 
   return (
-    <div className="flex bg-gray-900">
+    <div className="flex bg-gray-900 w-full h-full overflow-hidden">
       <HamburgerIcon />
       <div className="m-auto text-2xl text-gray-200 font-semibold p-2">
         {title}
